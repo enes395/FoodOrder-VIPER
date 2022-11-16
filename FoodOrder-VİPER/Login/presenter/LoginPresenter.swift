17b13434f1708ec578c2161a7bab60bf40eed926
Muhammed Enes <kiliclimuhammedenes@gmail.com>
2022-11-16 16:21:15 +0300

    update

---
+++ FoodOrder-VİPER/Login/presenter/LoginPresenter.swift
@@ -0,0 +1,21 @@
+//
+//  LoginPresenter.swift
+//  FoodOrder-VİPER
+//
+//  Created by Muhammed Enes Kılıçlı on 13.11.2022.
+//
+
+import Foundation
+
+class LoginPresenter : ViewToPresenterLoginProtocol {
+    var loginInteractor: PresenterToInteractorLoginProtocol?
+    
+    var loginView: PresenterToViewLoginProtocol?
+    
+    func login(email: String, password: String) {
+        <#code#>
+    }
+    
+    
+    
+}
---
+++ FoodOrder-VİPER/Login/router/LoginRouter.swift
@@ -0,0 +1,18 @@
+//
+//  LoginRouter.swift
+//  FoodOrder-VİPER
+//
+//  Created by Muhammed Enes Kılıçlı on 13.11.2022.
+//
+
+import Foundation
+
+class LoginRouter : PresenterToRouterLoginProtocol {
+    static func createModule(ref: LoginVC) {
+        let presenter = loginPresen
+        
+    }
+    
+    
+    
+}
